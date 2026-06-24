981aa11ca0ff007939e5d34c265972692f3b81c0
Prakhar Bhatia <prakhar@Prakhars-MacBook-Pro-2.local>
2026-06-24 06:49:41 +0530

    Configure package import optimizations under experimental key in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@splinetool/react-spline", "@splinetool/runtime"],
+  experimental: {
+    optimizePackageImports: ["lucide-react", "framer-motion"],
+  },
 };
 
 export default nextConfig;
